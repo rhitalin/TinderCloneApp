fcb72a3d7d14537d1ee5a15898917663aafdce45
Rita Chiorescu <chiorescurita@yahoo.com>
2022-06-25 16:04:38 -0400

    commit 3

--- src/TinderCards.js
+++ src/TinderCards.js
@@ -1,19 +1,46 @@
+import { SwipeDown } from "@mui/icons-material";
 import React, { useState } from "react";
+import TinderCard from "react-tinder-card";
 import "./TinderCards.css";
 
 function TinderCards() {
     const [people, setPeople] = useState([
         {
             name: "Elon Musk",
-            url: "https://m.media-amazon.com/images/M/MV5BMTI5NDY5NjU3NF5BMl5BanBnXkFtZTcwMzQ0MTMyMw@@._V1_UY1200_CR94,0,630,1200_AL_.jpg",
+            url: "https://upload.wikimedia.org/wikipedia/commons/thumb/3/34/Elon_Musk_Royal_Society_%28crop2%29.jpg/1200px-Elon_Musk_Royal_Society_%28crop2%29.jpg",
+        },
+        {
+            name: "Jeff Bezos",
+            url: "https://m.media-amazon.com/images/M/MV5BYTNlOGZhYzgtMmE3OC00Y2NiLWFhNWQtNzg5MjRhNTJhZGVmXkEyXkFqcGdeQXVyNzg5MzIyOA@@._V1_.jpg",
         },
     ]);
 
+    const swiped = (direction, nameToDelete) => {
+        console.log("removing: " + nameToDelete);
+    };
+
+    const outOfFrame = (name) => {
+        console.log(name + " left the screen!");
+    };
+
     return (
         <div className="tinderCards">
-            {people.map((person) => (
-                <h1>{person.name}</h1>
-            ))}
+            <div className="tinderCards_cardContainer">
+                {people.map((person) => (
+                    <TinderCard
+                        className="swipe"
+                        key={person.name}
+                        preventSwipe={["up", "down"]}
+                        onSwipe={(dir) => swiped(dir, person.name)}
+                        onCardLeftScreen={() => outOfFrame(person.name)}
+                    >
+                        <div style={{ backgroundImage: "url(" + person.url + ")" }} className="card">
+                            <h3>{person.name}</h3>
+                        </div>
+
+                    </TinderCard>
+                ))}
+            </div>
         </div>
     );
 }
