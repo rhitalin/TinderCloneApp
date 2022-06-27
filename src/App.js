ff7337ecbb0a6d11b0ddfbbb1811d2bfe2b4d2be
Rita Chiorescu <chiorescurita@yahoo.com>
2022-06-26 20:23:15 -0400

    ...

--- src/App.js
+++ src/App.js
@@ -1,19 +1,11 @@
 import React from "react";
-import "./App.css";
-import Header from "./Header";
-import TinderCards from "./TinderCards";
-import SwipeButtons from "./SwipeButtons";
+import './App.css';
 
 function App() {
   return (
     //BEM class naming convention
     <div className="app">
-      <Header />
-      
-      <TinderCards />
-
-      <SwipeButtons />
-
+      <h1>Building a Tinder App!</h1>
     </div>
   );
 }
